ee82e7f0e81410c46051cdde83592a2e1e323e2a
cristianosts <cristianosantosreal@gmail.com>
2024-11-11 09:45:43 -0300

    act

--- script.js
+++ script.js
@@ -3,5 +3,16 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
+    
     msg.innerHTML = (`Agora são ${hora} horas!`)
+    if (hora >= 0 && hora < 12) {
+           img.src = 'manha.png'
+           document.body.style.background = '#e4f7a2'
+    } else if (hora >= 12 && hora < 18) {
+           img.src = 'tarde.png'
+           document.body.style.background = '#fdbd93'
+    } else {
+           img.src = 'noite.png'
+           document.body.style.background = '#5868c2'
+    }
 }
